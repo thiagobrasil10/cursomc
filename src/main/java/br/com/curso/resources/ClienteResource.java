2e5f388564f1fdd89ed2b40b41e8b6281e8f64a3
thiagobrasil10 <thiagobrasil10@gmail.com>
2019-09-04 22:40:55 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/br/com/curso/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package br.com.curso.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.curso.domain.Cliente;
+import br.com.curso.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET )
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		Cliente obj = clienteService.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
