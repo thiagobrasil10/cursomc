e2cc211424d492fec03b7ed7f63690f8e1d8ba81
Thiago Brasil <thiagobrasil10@gmail.com>
2019-09-07 23:17:19 -0300

    Inserido uma nova Categoria com POST

--- src/main/java/br/com/curso/services/CategoriaService.java
+++ src/main/java/br/com/curso/services/CategoriaService.java
@@ -16,9 +16,14 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 
 	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);		
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-				+ ", Tipo "+Categoria.class.getName()));
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 
 }
