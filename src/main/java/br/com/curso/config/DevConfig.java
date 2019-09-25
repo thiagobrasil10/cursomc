522d030d29f5d87cb6c3151b44e08dfe6fd1b308
thiagobrasil10 <thiagobrasil10@gmail.com>
2019-09-24 21:05:30 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/curso/config/DevConfig.java
@@ -0,0 +1,34 @@
+package br.com.curso.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.curso.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService service;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		service.instantiateTestDatabase();
+		return true;
+	}
+
+}
